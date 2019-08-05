08ef752f7670c60eb2c4b54c5b923dc26fc010ba
Marija Trajkova <tmarija2008@yahoo.com>
2019-08-05 15:49:54 +0200

    Make main implement CommandLineRunner interface

--- src/main/java/com/homework/jpahibernate/JpaHibernateApplication.java
+++ src/main/java/com/homework/jpahibernate/JpaHibernateApplication.java
@@ -1,13 +1,18 @@
 package com.homework.jpahibernate;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaHibernateApplication {
+public class JpaHibernateApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHibernateApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
